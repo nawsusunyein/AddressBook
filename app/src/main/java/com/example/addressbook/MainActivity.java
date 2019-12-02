ae3d6b757aa3a71aff1b23b856b64f5f98019430
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-02 17:01:25 +0630

    add activity for main and intent

--- app/src/main/java/com/example/addressbook/MainActivity.java
+++ app/src/main/java/com/example/addressbook/MainActivity.java
@@ -2,13 +2,38 @@ package com.example.addressbook;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button btnReadAddress;
+    Button btnWriteAddress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnReadAddress = (Button) findViewById(R.id.btnReadAddress);
+        btnWriteAddress = (Button) findViewById(R.id.btnWriteAddress);
+
+        btnReadAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent readIntent = new Intent(MainActivity.this,ReadAddress.class);
+                startActivity(readIntent);
+            }
+        });
+
+        btnWriteAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent writeIntent = new Intent(MainActivity.this,WriteAddress.class);
+                startActivity(writeIntent);
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/example/addressbook/WriteAddress.java
@@ -0,0 +1,14 @@
+package com.example.addressbook;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class WriteAddress extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_write_address);
+    }
+}
