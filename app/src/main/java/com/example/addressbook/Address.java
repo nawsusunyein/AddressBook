cbaeb89a337d62a888b05476747e6e26f704d18d
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-02 17:30:37 +0630

    add layout for address add screen and set functions for clear value

---
+++ app/src/main/java/com/example/addressbook/Address.java
@@ -0,0 +1,13 @@
+package com.example.addressbook;
+
+public class Address {
+    String name;
+    String phone;
+    String address;
+
+    Address(String name,String phone,String address){
+        this.name = name;
+        this.phone = phone;
+        this.address = address;
+    }
+}
--- app/src/main/java/com/example/addressbook/WriteAddress.java
+++ app/src/main/java/com/example/addressbook/WriteAddress.java
@@ -3,12 +3,55 @@ package com.example.addressbook;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class WriteAddress extends AppCompatActivity {
+
+    EditText txtPersonName;
+    EditText txtPhoneNumber;
+    EditText txtAddress;
+
+    Button btnSaveAddress;
+    Button btnClearValues;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_address);
+        btnSaveAddress = (Button) findViewById(R.id.btnAddAddress);
+        btnClearValues = (Button) findViewById(R.id.btnCancel);
+
+        btnSaveAddress.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addUserValue();
+            }
+        });
+
+        btnClearValues.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clearTextValues();
+            }
+        });
+    }
+
+    private void registerTextfieldsById(){
+        txtPersonName = (EditText) findViewById(R.id.txtPersonName);
+        txtPhoneNumber = (EditText) findViewById(R.id.txtPhoneNumber);
+        txtAddress = (EditText) findViewById(R.id.txtAddress);
+    }
+
+    private void addUserValue(){
+        registerTextfieldsById();
+    }
+
+    private void clearTextValues(){
+        registerTextfieldsById();
+        txtPersonName.setText("");
+        txtPhoneNumber.setText("");
+        txtAddress.setText("");
     }
 }
