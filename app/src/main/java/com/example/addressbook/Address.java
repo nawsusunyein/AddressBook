42f8693375062314cbd4d67879b19aff241f73d4
nawsusunyein <nawsusunyein@techfun-myanmar.com>
2019-12-03 11:17:57 +0630

    add address model and layout design and add function for inserting data to realtime database

--- app/src/main/java/com/example/addressbook/Address.java
+++ app/src/main/java/com/example/addressbook/Address.java
@@ -1,13 +1,37 @@
 package com.example.addressbook;
+import com.google.firebase.database.IgnoreExtraProperties;
 
+@IgnoreExtraProperties
 public class Address {
     String name;
     String phone;
     String address;
 
-    Address(String name,String phone,String address){
+    public Address(){}
+    public Address(String name,String phone,String address){
         this.name = name;
         this.phone = phone;
         this.address = address;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+    public String getPhone(){
+        return phone;
+    }
+
+    public void setAddress(String address){
+        this.address = address;
+    }
+    public String getAddress(){
+        return address;
     }
 }
--- app/src/main/java/com/example/addressbook/WriteAddress.java
+++ app/src/main/java/com/example/addressbook/WriteAddress.java
@@ -1,11 +1,23 @@
 package com.example.addressbook;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.io.Console;
 
 public class WriteAddress extends AppCompatActivity {
 
@@ -16,12 +28,17 @@ public class WriteAddress extends AppCompatActivity {
     Button btnSaveAddress;
     Button btnClearValues;
 
+    ProgressBar progressBar;
+
+    private DatabaseReference mDatabase;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write_address);
         btnSaveAddress = (Button) findViewById(R.id.btnAddAddress);
         btnClearValues = (Button) findViewById(R.id.btnCancel);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
 
         btnSaveAddress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -45,7 +62,44 @@ public class WriteAddress extends AppCompatActivity {
     }
 
     private void addUserValue(){
+        progressBar.setVisibility(View.VISIBLE);
         registerTextfieldsById();
+        String personName = txtPersonName.getText().toString();
+        String phoneNumber = txtPhoneNumber.getText().toString();
+        String address = txtAddress.getText().toString();
+        Address addr = new Address(personName,phoneNumber,address);
+
+        if(!(addr.getAddress().isEmpty()) && !(addr.getName().isEmpty()) && !(addr.getPhone().isEmpty())){
+            FirebaseDatabase database = FirebaseDatabase.getInstance();
+            DatabaseReference ref = database.getReference().child("Address");
+            ref.push().setValue(addr).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void aVoid) {
+                    progressBar.setVisibility(View.GONE);
+
+                    Toast.makeText(WriteAddress.this,"Inserted data successfully",Toast.LENGTH_LONG).show();
+                    clearTextValues();
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    progressBar.setVisibility(View.GONE);
+                    Toast.makeText(WriteAddress.this,"Insert data failure",Toast.LENGTH_LONG).show();
+                }
+            });
+        }else{
+            progressBar.setVisibility(View.GONE);
+            Toast.makeText(WriteAddress.this,"Fill all data",Toast.LENGTH_LONG).show();
+        }
+
+
+      //Store values if you would like to save with custom id
+      /*  DatabaseReference ref = database.getReference().child("Address").child("0");
+        ref.child("Name").setValue(addr.getName());
+        ref.child("Phone").setValue(addr.getPhone());
+        ref.child("Address").setValue(addr.getAddress());*/
+
+
     }
 
     private void clearTextValues(){
